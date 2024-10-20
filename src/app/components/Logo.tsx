c60fc2e2cea747617d297bc1df2fbceb3b6a97b1
Gabriel Passaes <gabriel.bc88@gmail.com>
2024-10-20 19:41:16 -0300

    chore: update project

--- src/app/components/Logo.tsx
+++ src/app/components/Logo.tsx
@@ -1,12 +1,11 @@
-import React from "react";
+import React from 'react';
 import Image from 'next/image';
 
 export const Logo = () => (
-    <Image
-      src='/logo.svg'
-      alt='Logo'
-      width={90}
-      height={90}
-      className='cursor-pointer' 
-    />
+  <Image
+    src='/logo.svg'
+    alt='Logo'
+    width={90}
+    height={90}
+    className='cursor-pointer' />
 );
